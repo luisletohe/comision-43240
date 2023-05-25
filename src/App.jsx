853e3e34f4c1fd7239129da177c0ed0411a9acf4
luisletohe <119020089+luisletohe@users.noreply.github.com>
2023-05-25 07:47:43 -0300

    primero

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>estoy en app</h1>;
+}
+
+export default App;
---
+++ src/Tarjeta.jsx
@@ -0,0 +1,12 @@
+export function Tarjeta() {
+  let text = "hola como estas";
+  let edad = 22;
+
+  return (
+    <div>
+      <img src="" alt="" />
+      <h3>{text}</h3>
+      <h4>{edad}</h4>
+    </div>
+  );
+}
