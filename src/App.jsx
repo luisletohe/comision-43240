53f3b414ea0946049546e3acbb7d07dad286ed68
luisletohe <119020089+luisletohe@users.noreply.github.com>
2023-05-25 10:40:43 -0300

    segunto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <h1>estoy en app</h1>;
-}
+import Footer from "./Footer";
+import Home from "./Home";
+import { Navbar } from "./Navbar";
+
+const App = () => {
+  return (
+    <>
+      <h1>Este es el app</h1>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
